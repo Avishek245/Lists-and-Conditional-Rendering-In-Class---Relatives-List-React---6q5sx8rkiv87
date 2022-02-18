b11d861d7b20344c4a29d9603d7be67072858382
Avishek245 <96076589+Avishek245@users.noreply.github.com>
2022-02-18 23:11:54 +0530

    "Assignment9"

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      names: ["Anuj", "Gaurav", "Tushar", "Deepak"]
+    };
+  }
+  render() {
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          {this.state.names &&
+            this.state.names.map((item, index) => {
+              let temp = "relativeListItem" + { index };
+              return <li key={temp}>{item}</li>;
+            })}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
